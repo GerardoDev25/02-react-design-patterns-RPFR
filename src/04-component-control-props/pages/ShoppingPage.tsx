f0da856ca675753431480a3b6cd312b76c7e9d6f
Gerardo Miranda <gerardodev25@gmail.com>
2023-11-30 18:21:19 -0400

    chore(refector): :recycle: 013 Controlando el estado desde el padre
    modify the onProductCountChange to controll all the state fron the father component

--- src/04-component-control-props/pages/ShoppingPage.tsx
+++ src/04-component-control-props/pages/ShoppingPage.tsx
@@ -33,21 +33,35 @@ export const ShoppingPage = () => {
   }>({});
 
   const onProductCountChange = ({ count, product }: onChangeArgs) => {
-    console.log({ count });
     setShoppingCart((oldShoppingCart) => {
-      if (count == 0) {
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const { [product.id]: toDelate, ...rest } = oldShoppingCart;
-
-        return rest;
-      }
-
-      const newState = {
-        ...oldShoppingCart,
-        [product.id]: { ...product, count },
+      const productInCard: ProductInCart = oldShoppingCart[product.id] || {
+        ...product,
+        count: 0,
       };
 
-      return newState;
+      if (Math.max(productInCard.count + count, 0) > 0) {
+        productInCard.count += count;
+        return { ...oldShoppingCart, [product.id]: productInCard };
+      }
+
+      // ? borrar el producto del carrito
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { [product.id]: toDelate, ...rest } = oldShoppingCart;
+      return rest;
+
+      // if (count == 0) {
+      //   // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      //   const { [product.id]: toDelate, ...rest } = oldShoppingCart;
+
+      //   return rest;
+      // }
+
+      // const newState = {
+      //   ...oldShoppingCart,
+      //   [product.id]: { ...product, count },
+      // };
+
+      // return newState;
     });
   };
 
