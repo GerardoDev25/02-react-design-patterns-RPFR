69747acaaf437398b8c60c80ec915241806a38a7
Gerardo Miranda <gerardodev25@gmail.com>
2023-11-30 18:42:08 -0400

    refactor(refector): :recycle: 015 Respaldo de nuestra rama
    015 Respaldo de nuestra rama
    
    015 Respaldo de nuestra rama

--- src/04-component-control-props/hooks/usProduct.ts
+++ src/04-component-control-props/hooks/usProduct.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Product, onChangeArgs } from '../interfaces';
 
 interface useProductArgs {
@@ -11,17 +11,11 @@ export const useProduct = (props: useProductArgs) => {
   const { onChange, product, value = 0 } = props;
   const [counter, setCounter] = useState(value);
 
-  const isControlled = useRef(!!onChange);
-
   useEffect(() => {
     setCounter(value);
   }, [value]);
 
   const increaseBy = (value: number) => {
-    if (isControlled.current) {
-      return onChange!({ count: value, product });
-    }
-
     const newValue = Math.max(counter + value, 0);
     setCounter(newValue);
     onChange && onChange({ count: newValue, product });
--- src/04-component-control-props/hooks/useShoppingCart.ts
+++ src/04-component-control-props/hooks/useShoppingCart.ts
@@ -8,47 +8,46 @@ export const useShoppingCart = () => {
 
   const onProductCountChange = ({ count, product }: onChangeArgs) => {
     setShoppingCart((oldShoppingCart) => {
-      const productInCard: ProductInCart = oldShoppingCart[product.id] || {
-        ...product,
-        count: 0,
-      };
+      if (count == 0) {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const { [product.id]: toDelate, ...rest } = oldShoppingCart;
 
-      if (Math.max(productInCard.count + count, 0) > 0) {
-        productInCard.count += count;
-        return { ...oldShoppingCart, [product.id]: productInCard };
+        return rest;
       }
 
-      // ? borrar el producto del carrito
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const { [product.id]: toDelate, ...rest } = oldShoppingCart;
-      return rest;
+      const newState = {
+        ...oldShoppingCart,
+        [product.id]: { ...product, count },
+      };
+
+      return newState;
     });
   };
 
   return { shoppingCart, onProductCountChange };
 };
 
-// export const useShoppingCart2 = () => {
+// export const useShoppingCart = () => {
 //   const [shoppingCart, setShoppingCart] = useState<{
 //     [key: string]: ProductInCart;
 //   }>({});
 
 //   const onProductCountChange = ({ count, product }: onChangeArgs) => {
 //     setShoppingCart((oldShoppingCart) => {
-
-//       if (count == 0) {
-//         // eslint-disable-next-line @typescript-eslint/no-unused-vars
-//         const { [product.id]: toDelate, ...rest } = oldShoppingCart;
-
-//         return rest;
-//       }
-
-//       const newState = {
-//         ...oldShoppingCart,
-//         [product.id]: { ...product, count },
+//       const productInCard: ProductInCart = oldShoppingCart[product.id] || {
+//         ...product,
+//         count: 0,
 //       };
 
-//       return newState;
+//       if (Math.max(productInCard.count + count, 0) > 0) {
+//         productInCard.count += count;
+//         return { ...oldShoppingCart, [product.id]: productInCard };
+//       }
+
+//       // ? borrar el producto del carrito
+//       // eslint-disable-next-line @typescript-eslint/no-unused-vars
+//       const { [product.id]: toDelate, ...rest } = oldShoppingCart;
+//       return rest;
 //     });
 //   };
 
