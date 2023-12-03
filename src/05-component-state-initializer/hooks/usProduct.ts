004ef6abb44e981330b9bdceeb45cbe198f5372e
Gerardo Miranda <gerardodev25@gmail.com>
2023-12-03 15:00:36 -0400

    chore: :construction: 006 Utilizar el MaxCount como limitante
    limiting the max value that our counter can have (impementing the maxCount parameter)
    
    006 Utilizar el MaxCount como limitante

--- src/05-component-state-initializer/hooks/usProduct.ts
+++ src/05-component-state-initializer/hooks/usProduct.ts
@@ -13,6 +13,7 @@ export const useProduct = (props: useProductArgs) => {
   const [counter, setCounter] = useState<number>(initialValues?.count || value);
 
   const isMounted = useRef(false);
+
   useEffect(() => {
     isMounted.current = true;
   }, []);
@@ -23,7 +24,11 @@ export const useProduct = (props: useProductArgs) => {
   }, [value]);
 
   const increaseBy = (value: number) => {
-    const newValue = Math.max(counter + value, 0);
+    let newValue = Math.max(counter + value, 0);
+
+    if (initialValues?.maxCount)
+      newValue = Math.min(newValue, initialValues.maxCount);
+
     setCounter(newValue);
     onChange && onChange({ count: newValue, product });
   };
