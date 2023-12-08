815385af1b623cc8f38b7c6f24b64630b4393951
Gerardo Miranda <gerardodev25@gmail.com>
2023-12-07 20:19:46 -0400

    chore(Form): :construction: 005 Formulario tradicional - con useState
    005 Formulario tradicional - con useState
    
    005 Formulario tradicional - con useState

--- src/03-forms/pages/RegisterPage.tsx
+++ src/03-forms/pages/RegisterPage.tsx
@@ -1,14 +1,47 @@
+import { ChangeEvent, useState } from 'react';
 import '../style/style.css';
 
 export const RegisterPage = () => {
+  const [registerData, setRegisterData] = useState({
+    name: 'Gerardo',
+    email: 'gerado@gmail.com',
+    password1: '12345',
+    password2: '12345',
+  });
+  const { email, name, password1, password2 } = registerData;
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    console.log(event.target.value);
+  };
+
   return (
     <div>
       <h1>register Page</h1>
       <form>
-        <input type='text' placeholder='name' />
-        <input type='email' placeholder='email' />
-        <input type='password' placeholder='password' />
-        <input type='password' placeholder='repeat password' />
+        <input
+          onChange={onChange}
+          value={name}
+          type='text'
+          placeholder='name'
+        />
+        <input
+          onChange={onChange}
+          value={email}
+          type='email'
+          placeholder='email'
+        />
+        <input
+          onChange={(ev) => onChange(ev)}
+          value={password1}
+          type='password'
+          placeholder='password'
+        />
+        <input
+          onChange={onChange}
+          value={password2}
+          type='password'
+          placeholder='repeat password'
+        />
         <button type='submit'>Create</button>
       </form>
     </div>
