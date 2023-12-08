6fcfc02c303bcadaff98b220846db98d029918f8
Gerardo Miranda <gerardodev25@gmail.com>
2023-12-08 18:11:42 -0400

    chore(Form): :building_construction: 010 Formik - Obtener información del formulario
    starting work with formik
    
    v

--- src/03-forms/pages/FormikBasicPage.tsx
+++ src/03-forms/pages/FormikBasicPage.tsx
@@ -1,20 +1,50 @@
+import { useFormik } from 'formik';
 import '../style/style.css';
 export const FormikBasicPage = () => {
+  const { handleChange, values, handleSubmit } = useFormik({
+    initialValues: {
+      fistName: '',
+      lastName: '',
+      email: '',
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
   return (
     <div>
       <h1>Formik Basic Tutorial</h1>
 
-      <form noValidate>
+      <form noValidate onSubmit={handleSubmit}>
         <label htmlFor='fistName'>Firs Name</label>
-        <input type='text' name='fistName' id='fistName' />
+        <input
+          type='text'
+          name='fistName'
+          id='fistName'
+          value={values.fistName}
+          onChange={handleChange}
+        />
         <span>fist name is required</span>
 
         <label htmlFor='lastName'>Last Name</label>
-        <input type='text' name='lastName' id='lastName' />
+        <input
+          type='text'
+          name='lastName'
+          id='lastName'
+          value={values.lastName}
+          onChange={handleChange}
+        />
         <span>last name is required</span>
 
         <label htmlFor='email'>Email</label>
-        <input type='email' name='email' id='email' />
+        <input
+          type='email'
+          name='email'
+          id='email'
+          value={values.email}
+          onChange={handleChange}
+        />
         <span>email is required</span>
         <span>check for a valid email</span>
 
